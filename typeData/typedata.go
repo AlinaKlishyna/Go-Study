e6c35af82ad1aae066885504f4f2690adc162094
Alina <alinakotova102@gmail.com>
2025-10-25 15:02:31 +0200

    studing arithmetic operations

--- typeData/typedata.go
+++ typeData/typedata.go
@@ -11,7 +11,7 @@ var max, min float64
 var isActive = true
 var str = "I'm good!"
 
-// Для хранения символов принято использовать тип rune и int32
+// Для хранения символоclв принято использовать тип rune и int32
 var symbol int32 = 'f'
 var anotherSymbol rune = '?'
 
@@ -22,8 +22,20 @@ var defaultFloat float64 // 0.0
 var defaultBool bool     // false
 var defaultRune rune     // 0
 
+// Более краткий вывод переменной
+var short int = 5
+var anotherShort = 5
+
+// Можно обновить сразу несколько переменных в блоке var
+var (
+	name string = "Alina" // Не нужно писать тип переменной (var)
+	age  int    = 23
+)
+
 func main() {
 	fmt.Println(str)
 	fmt.Println(symbol)      // Выводит числовое значение символа
 	fmt.Println(string(102)) // Преобразует числовое значение в символ
+
+	fmt.Println(name, " - ", age)
 }
