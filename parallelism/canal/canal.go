030457d42d2b8745ddb6286ea9db5425ed65951d
Alina Klishyna <alinakotova102@gmail.com>
2025-11-04 16:28:54 +0100

    studing

--- parallelism/canal/canal.go
+++ parallelism/canal/canal.go
@@ -6,6 +6,79 @@ import (
 )
 
 func main() {
+	// example()
+	// example2
+	// task()
+	task3()
+}
+
+func removeDuplicates(input, output chan string) {
+	var prev string
+	for v := range input {
+		if v != prev {
+			output <- v
+			prev = v
+		}
+	}
+	close(output)
+}
+
+func printer(output chan string) {
+	for v := range output {
+		fmt.Println(v)
+	}
+}
+
+func task3() {
+	inputStream := make(chan string)
+	outputStream := make(chan string)
+
+	go removeDuplicates(inputStream, outputStream)
+	go printer(outputStream)
+
+	for _, i := range "112334456" {
+		inputStream <- string(i)
+	}
+
+	close(inputStream)
+}
+
+func task2(s chan string, stroke string) {
+	for i := 0; i < 5; i++ {
+		s <- stroke + " "
+	}
+}
+
+func task(c chan int, n int) {
+	c <- n + 1
+}
+
+func example2() {
+	fmt.Println("main() started")
+	c := make(chan string)
+
+	go greet(c)
+
+	c <- "John"
+
+	// periodic block/unblock of main goroutine until chanel closes
+	for {
+		if val, ok := <-c; !ok {
+			fmt.Println(val, ok, " <-- loop broke!")
+			break
+		}
+
+	}
+
+	fmt.Println("main() stopped")
+}
+
+func greet(c chan string) {
+	fmt.Println("Hello " + <-c + "!")
+	close(c)
+}
+
+func example() {
 	// Канал
 	message := make(chan string) // канал для строк
 
